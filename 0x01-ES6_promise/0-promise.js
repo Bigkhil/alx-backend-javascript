a4ee58d39507efb0b642248b1bbc0791256f0451
Bigkhil <khm40005000@outlook.com>
2024-10-05 17:19:38 +0300

    some changes

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,11 @@
 export default function getResponseFromAPI(){
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            const response = {
-                status: 200,
-                data: "connection established",
-            }
-            resolve(response);
-        }, 1000);
-    });
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			const response = {
+				status: 200,
+				data: "connection established",
+			}
+			resolve(response);
+		}, 1000);
+	});
 }
