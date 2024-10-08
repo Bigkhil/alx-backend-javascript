1251447663ff35fe55cf3fdcb7040f5819f4290c
Bigkhil <khm40005000@outlook.com>
2024-10-08 10:33:57 +0300

    some changes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -2,7 +2,7 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') throw new TypeError('Name must be a string');
     if (typeof length !== 'number') throw new TypeError('Length must be a number');
-    if (!Array.isArray(students) && students.every((student) => typeof student !== 'string')) throw new TypeError();
+    if (!Array.isArray(students) || students.every((student) => typeof student !== 'string')) throw new TypeError();
     this._name = name;
     this._length = length;
     this._students = students;
@@ -31,7 +31,7 @@ export default class HolbertonCourse {
   }
 
   set students(newstudents) {
-    if (!Array.isArray(students) && newstudents.every((student) => typeof student !== 'string')) throw new TypeError();
+    if (!Array.isArray(students) || newstudents.every((student) => typeof student !== 'string')) throw new TypeError();
     this._students = newstudents;
   }
 }
