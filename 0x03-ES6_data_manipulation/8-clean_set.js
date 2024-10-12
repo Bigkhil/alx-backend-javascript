947437a3a03b1d4661ab09aa08a85c051894491c
Bigkhil <khm40005000@outlook.com>
2024-10-12 17:45:59 +0300

    some changes

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(st, str) {
-  if (str === '') return '';
+  if (str === '' || !str) return '';
   const arr = Array.from(st);
   const filtered = arr.filter((ele) => ele.startsWith(str));
   let ret = filtered.reduce((acc, curr) => `${acc}-${curr.substring(str.length)}`, '');
